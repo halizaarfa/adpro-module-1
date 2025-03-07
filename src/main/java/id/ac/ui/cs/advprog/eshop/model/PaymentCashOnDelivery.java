9dab50043f4635977a4be21bd379c4875f283b02
halizaarfa <halizansa@gmail.com>
2025-03-07 08:34:11 +0700

    [GREEN] Implement PaymentCashOnDelivery

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentCashOnDelivery.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentCashOnDelivery.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import enums.PaymentStatus;
+
 import java.util.Map;
 
 public class PaymentCashOnDelivery extends Payment {
@@ -13,5 +15,11 @@ public class PaymentCashOnDelivery extends Payment {
 
     @Override
     public void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData.isEmpty() || paymentData.get("address").isEmpty() || paymentData.get("deliveryFee").isEmpty()) {
+            this.status = PaymentStatus.REJECTED.getValue();
+        } else {
+            this.paymentData = paymentData;
+            this.status = PaymentStatus.SUCCESS.getValue();
+        }
     }
 }
